b587bca293e33a4bb63e896d5e33591db93748a4
Silvio Netto <silvio.netto@gmail.com>
2016-05-12 21:32:20 +0200

    Adding get and set

--- src/main/java/com/rsvalerio/route/entity/Fees.java
+++ src/main/java/com/rsvalerio/route/entity/Fees.java
@@ -1,7 +1,47 @@
 package com.rsvalerio.route.entity;
 
+import java.util.Calendar;
+import java.util.Currency;
+
 /**
  * Created by silvionetto on 5/12/16.
  */
 public class Fees {
+
+    private int id;
+    private String type;
+    private Currency currency;
+    private Calendar date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
 }
--- src/main/java/com/rsvalerio/route/entity/Trade.java
+++ src/main/java/com/rsvalerio/route/entity/Trade.java
@@ -1,10 +1,56 @@
 package com.rsvalerio.route.entity;
 
+import java.util.Calendar;
+import java.util.Currency;
+
 /**
  * Created by silvionetto on 5/12/16.
  */
 public class Trade {
 
     private int id;
+    private Calendar date;
+    private Currency amount;
+    private LegalEntity legalEntity;
     private Vector<Fee> fees;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public Currency getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Currency amount) {
+        this.amount = amount;
+    }
+
+    public LegalEntity getLegalEntity() {
+        return legalEntity;
+    }
+
+    public void setLegalEntity(LegalEntity legalEntity) {
+        this.legalEntity = legalEntity;
+    }
+
+    public Vector<Fee> getFees() {
+        return fees;
+    }
+
+    public void setFees(Vector<Fee> fees) {
+        this.fees = fees;
+    }
 }
